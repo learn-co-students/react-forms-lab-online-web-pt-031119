bc337a915c50a82fb5bc853261cf4e90a7b08b13
“Mark <mkanko66@gmail.com>
2019-11-24 20:46:30 -0800

    lab completed

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,18 +3,39 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
+    
+    this.state = {
+      messageText: ''
+    };
+  }
 
-    this.state = {};
+  handleChange = (event) => {
+    this.setState({
+      messageText: event.target.value 
+    })
   }
 
   render() {
     return (
-      <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
+      <div>        
+          <strong>Remaining characters: </strong>
+          {this.props.maxChars - this.state.messageText.length}         
+        <div>
+          <br></br>  
+            <strong>Your message: </strong>
+            <input 
+            type="text" 
+            name="message" 
+            id="message" 
+            value={this.state.messageText} 
+            onChange={event => this.handleChange(event)} 
+            />             
+        </div>
+      </div>      
     );
   }
 }
 
 export default TwitterMessage;
+
+
